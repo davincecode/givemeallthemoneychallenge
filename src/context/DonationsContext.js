1cd3075e75e595a6f7610e93499ac1172ea08af8
davincecode <hyperscript.ca@gmail.com>
2022-07-13 05:52:29 -0600

    Added useContext hooks

---
+++ src/context/DonationsContext.js
@@ -0,0 +1,46 @@
+import { useState, createContext } from "react"
+
+export const DonationsContext = createContext()
+
+function DonationsContextProvider(props) {
+  const [amount, setAmount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const calcTotal = () => {
+    if (total >= 500) {
+      return "You have reached the goal!"
+    } else {
+      setTotal(amount + total)
+    }
+  }
+
+  const countUp = () => {
+    setCount(count + 1)
+  }
+
+  const fireClick = () => {
+    calcTotal()
+    countUp()
+  }
+
+  const remaining = 500 - total
+
+  const value = {
+    amount,
+    setAmount,
+    total,
+    calcTotal,
+    remaining,
+    count,
+    fireClick,
+  }
+
+  return (
+    <DonationsContext.Provider value={value}>
+      {props.children}
+    </DonationsContext.Provider>
+  )
+}
+
+export default DonationsContextProvider
