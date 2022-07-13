e697e4e33bbf2f5571213f4f3e017919682372f7
davincecode <hyperscript.ca@gmail.com>
2022-07-12 19:21:30 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Give me all the money!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
